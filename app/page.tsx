3b9a9f917d285694092f064c1dc1e1f219a2c841
yangwenxuan <540040588@qq.com>
2023-09-05 23:47:31 +0800

    updatge

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+'use client'
 import Image from 'next/image'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
 
 export default function Home() {
+  const isEmpty = true
+
+  if (isEmpty) {
+    return <EmptyState showReset/>
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'>Hello Airbnb!</div>
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-col-6 gap-8">
+        <div>My future listings</div>
+      </div>
+    </Container>
   )
 }
